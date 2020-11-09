d286766e3f73da71644f8f5f37ba1d689fa1a3aa
Shrey Joshi <shreyjoshi2004@gmail.com>
2020-11-09 00:31:53 -0600

    add makeheaders file and rename dir

--- caleb-yt/operator_overload.cpp
+++ caleb-yt/operator_overload.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 #include <string>
 
+
 class Coordinate
 {
+    std::string status = "made";
     public:
         int x = 10;
         int y = 10;
@@ -22,17 +24,31 @@ class Coordinate
             }
             return false;
         }
+
+        // friend method in public part of class. We declare it here but define outside of class.
+        friend void output_status(Coordinate coord);
+        friend std::ostream& operator << (std::ostream& output, Coordinate a);
 };
 
+
+// standalone function --> not a member function or method
+void output_status(Coordinate coord)
+{
+    std::cout << coord.status;
+}
+
+
+// overload << operator for class Coordinate
+// since it's a friend function we can access private variable status
 std::ostream& operator << (std::ostream& output, Coordinate a)
 {
-    return output << "(" << a.x << ", " << a.y << ")";
+    return output << "(" << a.x << ", " << a.y << ")" << " with status: " << a.status;
 }
+
 
 int main()
 {
     Coordinate a, b;
-
     Coordinate c = a + b;
 
     // this next line is without << operator overloading
@@ -40,8 +56,8 @@ int main()
 
     // this is with << operator overloading
     std::cout << c << std::endl;
-    
     std::cout << std::boolalpha << (a == b) << std::endl;
-
     std::cout << a << std::endl;
+
+    output_status(a);
 }
